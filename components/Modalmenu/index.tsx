3c63470e3c16334bd4cd863660ed280326bd9cc2
Kalita Trevisan <kalitatrevi@icloud.com>
2024-11-26 22:11:40 -0300

    modal

---
+++ components/Modalmenu/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text } from "react-native";
+const Modalmenu = () => {
+  return (
+    <View>
+      <View>
+        <Text>Modalmenu</Text>
+      </View>
+
+      <View>
+        <Text>Modalmenu</Text>
+      </View>
+
+      <View>
+        <Text>Modalmenu</Text>
+      </View>
+      <View>
+        <Text>Modalmenu</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Modalmenu;
